c9f4b21555e904811063ac45d8e2c27bb4b06a3e
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2024-10-26 17:35:42 +0530

    Footer

--- grocery/app/page.js
+++ grocery/app/page.js
@@ -4,6 +4,8 @@ import Slider from "./_components/Slider";
 import GlobalApi from "./_utils/GlobalApi";
 import CategoryList from "./_components/CategoryList";
 import ProductList from "./_components/ProductList";
+import Banner from "../public/banner.png"
+import Footer from "./_components/Footer";
 
 export default async function Home() {
 
@@ -18,6 +20,11 @@ export default async function Home() {
       <CategoryList categoryList={categoryList} />
 
       <ProductList productList={productList}/>
+
+      {/* Banner */}
+      <Image src={Banner}  alt="Banner" width={1000} height={300} className="w-full h-[400px] object-contain mt-5" />
+
+      <Footer />
     </div>
   );
 }
